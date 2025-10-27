0eae11dfe5fbfb61f1e56bee523f7756942026e6
Ruoqi Huang <huangruoqi374@gmail.com>
2025-10-26 18:11:07 -0700

    cool fix

--- bug.py
+++ bug.py
@@ -1,6 +1,23 @@
 import tokenize
 import io
+import dis
 
-source = "def f():\n" "\tif x\n" '\x00'
-fp = io.StringIO(source)
-list(tokenize.generate_tokens(fp.readline))
+def a():
+    source = "def f():\n" "\tif x\n" '\x00' '0000000000'
+    fp = io.StringIO(source)
+    list(tokenize.generate_tokens(fp.readline))
+
+
+a()
+
+'''
+FIX:
+    lexer.c to add a final check error at EOF to force raise Error:
+
+    ```
+            else if (c == EOF && PyErr_Occurred()) {
+                return MAKE_TOKEN(ERRORTOKEN);
+            }
+    ```
+
+'''
